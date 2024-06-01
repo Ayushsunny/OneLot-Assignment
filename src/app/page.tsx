b866fcc4a81560e79ab9e07178ca9f44623833be
Ayushsunny <ayush100anand@gmail.com>
2024-06-02 00:37:20 +0530

    Hero Section Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
-    <div className ="okay"></div>
+    <div className ="okay">
+      <Hero />
+    </div>
   )
 }
